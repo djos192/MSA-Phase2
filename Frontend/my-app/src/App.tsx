d7eb3f5fddbcc23da59522e8ae9fca2586b489d0
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2022-08-09 23:10:28 +1200

    added API call functionality

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,32 +1,39 @@
-import { useState } from 'react';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
 
   const [pokemonName, setPokemonName] = useState("");
 
+  const POKEMON_BASE_URL = "https://pokeapi.co/api/v2";
   return (
     <div>
-      <h1>
-        Pokemon Search
-      </h1>
-      
+      <h1>Pokemon Search</h1>
+
       <div>
-        <label>Pokemon Name</label><br/>
-        <input type="text" id="pokemon-name" name="pokemon-name" onChange={e => setPokemonName(e.target.value)}/><br/>
-        <button onClick={search}>
-        Search
-        </button>
+        <label>Pokemon Name</label>
+        <br />
+        <input
+          type="text"
+          id="pokemon-name"
+          name="pokemon-name"
+          onChange={(e) => setPokemonName(e.target.value)}
+        />
+        <br />
+        <button onClick={search}>Search</button>
       </div>
 
-      <p>
-        You have entered {pokemonName}
-      </p>
+      <p>You have entered {pokemonName}</p>
+
+      <div id="pokemon-result">This will show the result</div>
     </div>
   );
 
-  function search(){
-      alert("Search button has been clicked!");
+  function search() {
+    axios.get(POKEMON_BASE_URL + "/pokemon/" + pokemonName).then((res) => {
+      console.log(res.data);
+    });
   }
 }
 
