0190437af0a418f1842df99641542d44d7970c83
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2022-08-09 23:23:55 +1200

    Implemented typescript interface

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,12 +1,14 @@
 import axios from "axios";
+import { Pokemon } from "pokenode-ts";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
 
   const [pokemonName, setPokemonName] = useState("");
+  const [pokemonInfo, setPokemonInfo] = useState<undefined | Pokemon>(undefined);
 
-  const POKEMON_BASE_URL = "https://pokeapi.co/api/v2";
+  const POKEMON_BASE_API_URL = "https://pokeapi.co/api/v2";
   return (
     <div>
       <h1>Pokemon Search</h1>
@@ -26,13 +28,19 @@ function App() {
 
       <p>You have entered {pokemonName}</p>
 
-      <div id="pokemon-result">This will show the result</div>
+      {pokemonInfo === undefined || pokemonInfo.sprites.other.dream_world.front_default === null? (
+        <p>Pokemon not found</p>
+      ) : (
+        <div id="pokemon-result">
+          <img src={pokemonInfo.sprites.other.dream_world.front_default} />
+        </div>
+      )}
     </div>
   );
 
   function search() {
-    axios.get(POKEMON_BASE_URL + "/pokemon/" + pokemonName).then((res) => {
-      console.log(res.data);
+    axios.get(POKEMON_BASE_API_URL + "/pokemon/" + pokemonName).then((res) => {
+      setPokemonInfo(res.data);
     });
   }
 }
