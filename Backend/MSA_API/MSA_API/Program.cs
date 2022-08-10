c3a16b9ce4c82f27b413c4573ef3e2153e91b4c4
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2022-08-10 13:21:12 +1200

    Initiated Swagger

--- Backend/MSA_API/MSA_API/Program.cs
+++ Backend/MSA_API/MSA_API/Program.cs
@@ -4,15 +4,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.UseSwagger(options =>
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    options.SerializeAsV2 = true;
+});
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+builder.Services.AddSwaggerDocument(options =>
+{
+    options.DocumentName = "DJs API";
+    options.Version = "V1";
+});
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+
+{
+    app.UseDeveloperExceptionPage();
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
 }
 
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -23,3 +45,8 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+
+
+
